1351fc8149b7c9425d7b95f7b8f794955d4e350e
nssriwulandari <nssriwulandari@gmail.com>
2024-07-08 09:17:40 +0000

    belajar flutter (container & stack)

---
+++ lib/container_widget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class BelajarContainer extends StatelessWidget {
+  const BelajarContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.only(left:10),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(197, 230, 39, 156),
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: Colors.black,
+          width: 5,
+        ),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://picsum.photos/200/300'),
+            fit: BoxFit.cover
+          ),
+        ),
+        child: Text('Hallo'),
+          ),
+        );
+  }
+}
+  
